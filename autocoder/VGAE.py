ba086b4fe46bc2b7e407a7cdda187ac4dec271dc
sjt5285126 <545446860@qq.com>
2021-11-04 15:12:32 +0800

    增加Relu()函数 line 14 19

--- autocoder/VGAE.py
+++ autocoder/VGAE.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 import torch_geometric
 import torch_geometric.nn as gnn
 from torch_geometric.nn.models.autoencoder import VGAE
@@ -10,11 +11,15 @@ class VGAE_encoder(nn.Module):
         self.conv1 = gnn.GCNConv(in_channels,2*out_channels,cached=True)
         self.conv_mu = gnn.GCNConv(2*out_channels,out_channels,cached=True)
         self.conv_logvar = gnn.GCNConv(2*out_channels,out_channels,cached=True)
+        self.relu = nn.ReLU() #nn.Relu() 相当于一个模型,声明后才可使用
+
 
     def forward(self,x,edge_index):
-        x = nn.ReLU(self.conv1(x,edge_index))
+
+        x = self.relu(self.conv1(x,edge_index))
         # 在中间可以加入dropout操作
         # x = nn.Dropout(x)
         mu = self.conv_mu(x,edge_index)
         logvar = self.conv_logvar(x,edge_index)
+        return mu,logvar
 

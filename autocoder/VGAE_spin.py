94fe605ac547e68e080a4bf1aa8af7c51d89bc5d
sjt5285126 <545446860@qq.com>
2021-11-29 20:46:31 +0800

    初始模型修改成功

--- autocoder/VGAE_spin.py
+++ autocoder/VGAE_spin.py
@@ -1,13 +1,15 @@
+import pickle
+import torch_geometric.data as gdata
+import torch_geometric.loader as gloader
 import torch
 import numpy as np
 import torch.nn as nn
 import torch.nn.functional as F
 import torch_geometric.nn as gnn
 from torch_geometric.nn.models.autoencoder import VGAE,GAE
+
 from VGAE import VGAE_encoder as vgae
-from torch_geometric.datasets import Planetoid
 import torch_geometric.transforms as T
-from torch_geometric.utils import train_test_split_edges
 import os.path as osp
 import dataset
 
@@ -30,6 +32,7 @@ class EncoderSpin(nn.Module):
 
 class DecoderSpin(nn.Module):
     def __init__(self):
+        super(DecoderSpin, self).__init__()
         self.decoder = nn.Sequential(
             nn.Linear(16,8),
             nn.ReLU(),
@@ -52,9 +55,32 @@ class SVGAE(VGAE):
 
     def recon_loss(self,x,x_):
         size = x.shape[0]
-        loss = self.loss(x_,self.flatten(x).to(torch.long))+self.kl_loss()/size
+        loss = self.loss(x_,self.flatten(x).to(torch.long)) + self.kl_loss()
         return loss
 
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+model = SVGAE(EncoderSpin(),DecoderSpin()).to(device)
+print(model)
+datafile = open('data/IsingGraph/data3.pkl','rb')
+data = pickle.load(datafile)
+datafile.close()
+print(data)
+data_train_batchs = gloader.DataLoader(data,batch_size=1,shuffle=True)
+
+optim = torch.optim.Adam(model.parameters(),lr=0.01)
+
+for epoch in range(100):
+    model.train()
+    for d in data_train_batchs:
+        d = d.to(device)
+        z = model.encode(d.x,d.edge_index)
+        x_ = model.decode(z)
+        optim.zero_grad()
+        loss = model.recon_loss(d.x,x_)
+        print('loss:{}'.format(loss))
+        loss.backward()
+        optim.step()
 
 
 

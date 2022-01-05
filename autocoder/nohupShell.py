0d369c4f485c0efd8321087c56808d3f4581d46a
sjt5285126 <545446860@qq.com>
2022-01-05 20:02:19 +0800

    修改错误

--- autocoder/nohupShell.py
+++ autocoder/nohupShell.py
@@ -1,15 +1,14 @@
 import os
 from sys import argv
-import dataInit
 
 if len(argv) < 6:
     print("please input: python nohupShell.py size T_list nums name times")
 
-size = int(argv[1])
-T_list = eval(argv[2])
-nums = int(argv[3])
+size = argv[1]
+T_list = argv[2]
+nums = argv[3]
 name = argv[4]
-times = int(argv[5]) # 次数表示一共有多少个批次
+times = argv[5] # 次数表示一共有多少个批次
 
 for i in range(1,times+1):
     os.system('nohup python dataInit.py {0} {1} {2} {3}_{4} > sizeLog/{3}_{4}.log 2>&1 &'

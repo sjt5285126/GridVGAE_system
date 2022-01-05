b9f28526cb4820f3948952ce40b7da2de0e777d8
sjt5285126 <545446860@qq.com>
2022-01-05 19:29:36 +0800

    完善代码

--- autocoder/nohupShell.py
+++ autocoder/nohupShell.py
@@ -2,13 +2,16 @@ import os
 from sys import argv
 import dataInit
 
+if len(argv) < 6:
+    print("please input: python nohupShell.py size T_list nums name times")
+
 size = int(argv[1])
 T_list = eval(argv[2])
 nums = int(argv[3])
 name = argv[4]
-time = int(argv[5]) # 次数表示一共有多少个批次
+times = int(argv[5]) # 次数表示一共有多少个批次
 
-for i in range(1,time+1):
+for i in range(1,times+1):
     os.system('nohup python dataInit.py {0} {1} {2} {3}_{4} > sizeLog/{3}_{4}.log 2>&1 &'
               .format(size,T_list,nums,name,i))
 

0b0d13539f3d6866c9616046b53f27d21018c15d
sjt5285126 <545446860@qq.com>
2022-01-06 10:28:12 +0800

    修改模型结构

--- autocoder/nohupShell.py
+++ autocoder/nohupShell.py
@@ -8,7 +8,7 @@ size = argv[1]
 T_list = argv[2]
 nums = argv[3]
 name = argv[4]
-times = argv[5] # 次数表示一共有多少个批次
+times = int(argv[5]) # 次数表示一共有多少个批次
 
 for i in range(1,times+1):
     os.system('nohup python dataInit.py {0} {1} {2} {3}_{4} > sizeLog/{3}_{4}.log 2>&1 &'

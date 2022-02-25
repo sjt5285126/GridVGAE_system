67919d106d554bd62cdf576609fab594038a1d9b
sjt5285126 <545446860@qq.com>
2022-02-25 10:05:27 +0800

    增加测试模块

--- autocoder/eval.py
+++ autocoder/eval.py
@@ -8,6 +8,13 @@ import h5py
 
 # 我们在知道mu与log的情况下，可以通过model的reparametrize()函数来得到z,再将z进行decode解码
 
+#　增加在训练中观察训练出的模型相似度的代码
+'''
+想法1:
+    1. acc = func(x_ - x) / nums_node
+    2. 将得到的结果进行归一化 mean(acc)
+    3. print('准确率{}'.format(acc * 100))
+'''
 def load_checkpoint(model, checkpoint_PATH, optimizer):
     if checkpoint_PATH != None:
         model_CKPT = torch.load(checkpoint_PATH)
@@ -18,8 +25,9 @@ def load_checkpoint(model, checkpoint_PATH, optimizer):
         optimizer.load_state_dict(model_CKPT['optimizer'])
         mu = model_CKPT['mu']
         log = model_CKPT['log']
+        batch_size = model_CKPT['batch_size']
     # 返回模型，优化器
-    return model, optimizer,mu,log
+    return model, optimizer,mu,log,batch_size
 
 device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 model = SVGAE(EncoderSpin(), DecoderSpin()).to(device)
@@ -28,15 +36,15 @@ PATH = 'model_16_220224.pkl'
 
 checkpoint = torch.load(PATH)
 
-model, optim,mu,log = load_checkpoint(model, PATH, optim)
+model, optim,mu,log,batch_size = load_checkpoint(model, PATH, optim)
 
 def reparametrize(mu,log):
     # 返回重采样的z
     return mu + torch.randn_like(log) + torch.exp(log)
 
-epochs = 1000
+epochs = 1
 
 for epoch in range(epochs):
     model.eval()
     z = reparametrize(mu,log)
-    print(z.shape)
+    print(z.shape[0]/batch_size)

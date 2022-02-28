e9a104d9b82a76eb6e3d513f6822652f4f6b8c85
qhk <545446860@qq.com>
2022-02-28 20:44:05 +0800

    更改模型，保存上次的模型的

--- autocoder/VGAE_spin.py
+++ autocoder/VGAE_spin.py
@@ -67,7 +67,7 @@ class DecoderSpin(nn.Module):
             nn.Linear(8, 4),
             nn.ReLU(),
             nn.Linear(4, 2),
-            nn.Sigmoid()
+            nn.Softmax(dim=-1)
         )
 
     def forward(self, z):

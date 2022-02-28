e1a54bee387933b4d3852525d86f3ed43bdb1d98
sjt5285126 <545446860@qq.com>
2022-02-28 16:17:49 +0800

    增加测试模块

--- autocoder/train.py
+++ autocoder/train.py
@@ -46,7 +46,7 @@ for epoch in range(1000):
             batch = batch.to(device)
             z = model.encode(batch.x,batch.edge_index,batch.edge_attr,batch.batch)
             x_ = model.decode(z)
-	    print("loss:{}".format(model.recon_loss(batch.x,x_) + model.kl_loss() / (batch.num_nodes/2)))
+            print("loss:{}".format(model.recon_loss(batch.x,x_) + model.kl_loss() / (batch.num_nodes/2)))
             print("测试构型:{}".format(reshapeIsing_MSE(batch.x,2)))
             print("重构后的构型:{}".format(reshapeIsing_MSE(x_,2)))
             print("acc:{}%".format(acc(batch.x,x_,2)))
